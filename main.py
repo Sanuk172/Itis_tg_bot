6f9f7c93fccdc909090f131da03d55db61ff312d
1 <vaasovsasa1w3@gmail.com>
2025-11-16 19:54:03 +0500

    first_work

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print('Hello')
-print('Hello world')
-print('Hello world1')
+from google import genai
+from Api_key import *
+
+client = genai.Client(api_key=GOOGLE_API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Напиши задачу по математике для тренировки 19 номера егэ по математике с ответом, без форматирования"
+)
+
+print(response.text)
